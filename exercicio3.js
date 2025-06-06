90e38af78440f31cca8f3067ce87a49e3a33d580
emilio <favorettoemilio@gmail.com>
2025-06-06 16:07:45 -0300

    verificarparouimpar

---
+++ exercicio3.js
@@ -0,0 +1,13 @@
+function VerificarParOuImpar (n) {
+    if(n % 2 === 0) {
+        console.log(true)
+    }else {
+        console.log(false)
+    }
+    
+    resultado = n
+    return resultado
+
+}
+
+VerificarParOuImpar(10)
