c2d815b002eb1759aab3501aa2412cbb79203d68
Victor Oliveira <victorcriri313@gmail.com>
2025-06-06 15:50:39 -0300

    soma array

--- soma-array.js
+++ soma-array.js
@@ -1,10 +1,10 @@
 function somaArray(array) {
     let soma = 0;
 
-    for (let i = 0; i < array.length; i++) {
+    for(let i = 0; i < array.length; i++) {
         soma = soma + array[i];
     }
     return soma;
 }
 
-console.log(somaArray([5, 4, 1, 4, 10, 20]));
+console.log(somaArray([1, 10, 6, 12, 16]));
